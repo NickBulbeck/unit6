5dcfc55f0796c70d8da5c0c283b717469b13826c
NickBulbeck <bulbecknick@gmail.com>
2020-08-18 15:36:48 +0100

    Added chai

--- npmProject/app.js
+++ npmProject/app.js
@@ -1,1 +1,3 @@
-// Outer function for the app...
+// Part 1: retrieve the data, and log out the body of the response.
+
+// Write the function that calls all the others. Oh, and require stuff in.
---
+++ npmProject/appTest.js
@@ -0,0 +1,22 @@
+const expect = require('chai').expect;
+
+// expect(true).to.be.true;
+
+
+// const titleCase = (title) => {
+// 	let workingTitle = title.toLowerCase();
+// 	let words = workingTitle.split(' ');
+// 	let titleCasedWords = words.map(function(word) {
+// 		return word[0].toUpperCase() + word.substring(1);
+// 	})
+// 	return titleCasedWords.join(' ');
+// }
+
+// expect(titleCase('the great mouse detective')).to.be.a('string');
+// expect(titleCase('t')).to.equal('T');
+// expect(titleCase('alien')).to.equal('Alien');
+// expect(titleCase('ALIEN')).to.equal('Alien');
+
+// expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
+
+console.log("appTest.js is working! (Or, actually, *npm test* is working!)");
